43c2e08f4ce01b300de05410f3920c305db34c22
incredible92 <sulymanabdullahi@gmail.com>
2021-01-13 11:49:29 +0100

    fix: Cv-class

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log('cv class')
+
+const rectangle = {
+    width: 10,
+    heigh: 20,
+    location: { x:0, y: 1 },
+    draw() {
+        console.log(`drawing...spec => width: ${this.width} height:${this.height} `)
+    }
+}
